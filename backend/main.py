e1d97d3b6a7138d45b1143123532dcf3c1816f3a
GURUDEV <gurudev1503l@gmail.com>
2026-08-10 15:02:21 +0530

    Add user API and database integration

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database import SessionLocal
 from models import User
+from schemas import UserCreate
 
 
 app = FastAPI()
@@ -20,3 +21,23 @@ def get_users():
     db.close()
 
     return users
+
+
+@app.post("/users")
+def create_user(user: UserCreate):
+    db = SessionLocal()
+
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=user.password,
+        role=user.role
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    db.close()
+
+    return new_user
