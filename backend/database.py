a98096a863d840cc24762067624531644d05b2a6
GURUDEV <gurudev1503l@gmail.com>
2026-08-10 14:51:53 +0530

    Add SQLAlchemy user model and users API

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,20 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 
 DATABASE_URL = "postgresql://postgres:InternMatch%40123@localhost:5432/internmatch"
 
+
 engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 
 with engine.connect() as connection:
     print("Database connected successfully!")
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from database import SessionLocal
+from models import User
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"message": "InternMatch Backend is running"}
+
+
+@app.get("/users")
+def get_users():
+    db = SessionLocal()
+
+    users = db.query(User).all()
+
+    db.close()
+
+    return users
